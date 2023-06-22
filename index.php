ceb1ce931e24dc06f8115c315a4c75b622142385
Joshua <joshuahufflepuff@gmail.com>
2023-06-22 17:47:42 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <base href="http://localhost/shoeit-app/">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Joshua">
+    <meta name="description" content="We are a shoe brand">
+    <meta name="keywords" content="shoes, brand new, branded, original">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Shoe It</title>
+</head>
+
+<body>
+    <main>
+        <nav>
+            <a class="push-right" href="index.php">
+                <img src="assets/img/logo.png" alt="Shoe It Logo">
+            </a>
+            <a href="index.php">Home</a>
+            <a href="#gallery">Gallery</a>
+            <a href="#">About Us</a>
+            <a href="#">Contact Us</a>
+        </nav>
+        <header>
+            <h1>Shoeeees It.</h1>
+            <p>Don't hide it - shoe it</p>
+        </header>
+        <section class="gallery">
+            <h1 id="gallery">Gallery</h1>
+            <div class="box-container">
+                <div class="box">
+                    <img src="assets/img/img1.jpeg" alt="Shoe 1">
+                    <p>Shoe 1</p>
+                </div>
+                <div class="box">
+                    <img src="assets/img/img2.jpeg" alt="Shoe 2">
+                    <p>Shoe 2</p>
+                </div>
+                <div class="box">
+                    <img src="assets/img/img3.jpeg" alt="Shoe 3">
+                    <p>Shoe 3</p>
+                </div>
+                <div class="box">
+                    <img src="assets/img/img4.jpeg" alt="Shoe 4">
+                    <p>Shoe 4</p>
+                </div>
+                <div class="box">
+                    <img src="assets/img/img5.jpeg" alt="Shoe 5">
+                    <p>Shoe 5</p>
+                </div>
+                <div class="box">
+                    <img src="assets/img/img6.jpeg" alt="Shoe 6">
+                    <p>Shoe 6</p>
+                </div>
+            </div>
+        </section>
+        <footer>&copy;Shoe It, 2023. All Rights Reserved</footer>
+    </main>
+</body>
+
+</html>
